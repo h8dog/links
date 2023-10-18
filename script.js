9aac953022e97a1d61f6424a97092a28c61b0059
h8dog <p4wz3r@gmail.com>
2023-10-18 23:45:39 +0000

    added files

---
+++ script.js
@@ -0,0 +1,8 @@
+function commissions(){
+    var comms = document.getElementById("commsBlock");
+    if (comms.style.display === "block") {
+        comms.style.display = "none";
+    } else {
+        comms.style.display = "block";
+    }
+}
