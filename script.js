42fb4da34faa6c976938763b5882be8802fe3ea9
h8dog <p4wz3r@gmail.com>
2023-11-01 21:10:11 +0000

    New background, improved js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function commissions(){
-    var comms = document.getElementById("commsBlock");
+function sectionOperation(section){
+    var comms = document.getElementById(section);
     if (comms.style.display === "block") {
         comms.style.display = "none";
     } else {
         comms.style.display = "block";
     }
 }
+
+function link(){
+    var body = document.querySelector('body');
+    var width = window.screen.width;
+    var link = 'https://images.unsplash.com/photo-1520491286939-1680f46efe91?auto=format&w='+width;
+    link = 'url('+link+')';
+    body.style.backgroundImage = `${link}`;
+}
+
+window.onload = link;
+window.onresize = link;
