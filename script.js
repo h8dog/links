6b288a4c1578b69101a07ad7be3021298cb8b983
h8dog <p4wz3r@gmail.com>
2023-11-23 23:50:16 +0000

    better image preview

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ function sectionOperation(open, close){
     } else {
         open.style.display = "block";
     }
-    if (close.style.display === "block") {
+    if (close.style.display === "block" && close != null) {
         close.style.display = "none";
     }
+}
+
+function gallery(image){
+    var image = image;
+    var preview = document.getElementById('preview');
+    var bg = document.getElementById('prevBg');
+    preview.style.opacity = 100;
+    preview.style.display = "block";
+    bg.style.opacity = 100;
+    bg.style.display = "block";
+    preview.src = image;
+}
+
+function preview(){
+    var preview = document.getElementById('preview');
+    var bg = document.getElementById('prevBg');
+    preview.style.opacity = 0;
+    preview.style.display = "none";
+    bg.style.opacity = 0;
+    bg.style.display = "none";
 }
